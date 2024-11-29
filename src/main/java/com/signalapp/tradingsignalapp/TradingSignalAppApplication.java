14366cfce11f2848dcb357dfc0b0ce9259aa42c9
Luki <bukaszlilski@gmail.com>
2024-11-29 18:39:23 +0100

    .

---
+++ src/main/java/com/signalapp/tradingsignalapp/TradingSignalAppApplication.java
@@ -0,0 +1,13 @@
+package com.signalapp.tradingsignalapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingSignalAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingSignalAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/signalapp/tradingsignalapp/TradingSignalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.signalapp.tradingsignalapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingSignalAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
