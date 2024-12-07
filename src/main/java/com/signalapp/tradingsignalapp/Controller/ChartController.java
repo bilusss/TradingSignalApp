dc4dfa96f87a1177e20750d090a5c383eead2033
Łukasz Bilski <bukaszlilski@gmail.com>
2024-12-08 00:21:46 +0100

    -BinanceWebSocket.java edit: ChartController.java = enabled passing historicalData into chart.html file

--- src/main/java/com/signalapp/tradingsignalapp/Controller/ChartController.java
+++ src/main/java/com/signalapp/tradingsignalapp/Controller/ChartController.java
@@ -1,5 +1,7 @@
 package com.signalapp.tradingsignalapp.Controller;
 
+import com.signalapp.tradingsignalapp.Service.BinanceHistoricalData;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,20 +13,30 @@ import java.util.List;
 @Controller
 public class ChartController {
     private final List<String> AvailablePairs;
+    private final BinanceHistoricalData binanceHistoricalData;
 
     @Autowired
-    public ChartController(List<String> AvailablePairs) {
+    public ChartController(List<String> AvailablePairs, BinanceHistoricalData binanceHistoricalData) {
         this.AvailablePairs = AvailablePairs;
+        this.binanceHistoricalData = binanceHistoricalData;
     }
 
-    @GetMapping("/chart/{pair}")
-    public String getChart(@PathVariable String pair, Model model) {
-        if (AvailablePairs.contains(pair)) {
-            model.addAttribute("pair", pair);
-            return "chart";
-        } else {
-            model.addAttribute("error", "Trading pair " + pair + " is not available.");
+    @GetMapping("/chart/{symbol}")
+    public String getChart(@PathVariable String symbol, Model model) {
+        if (!AvailablePairs.contains(symbol)) {
+            model.addAttribute("error", "Trading pair " + symbol + " is not available.");
             return "error";
         }
+        String interval = "15m";// Default interval
+        var historicalData = binanceHistoricalData.getHistoricalData(symbol, interval);
+
+        if (historicalData.isEmpty()) {
+            model.addAttribute("error", "Failed to load historical data for symbol: " + symbol);
+            return "error";
+        }
+
+        model.addAttribute("pair", symbol);
+        model.addAttribute("historicalData", historicalData);
+        return "chart";
     }
 }
