b25efe8e8031b6703c854b4ab097b104dbbf37a0
Łukasz Bilski <bukaszlilski@gmail.com>
2024-12-25 03:04:36 +0100

    Crypto all done

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,20 +1,8 @@
-CREATE TABLE IF NOT EXISTS "User" (
+CREATE TABLE IF NOT EXISTS "Users" (
     id SERIAL PRIMARY KEY,
     username varchar(250) NOT NULL,
     hash varchar(250) NOT NULL,
     credit FLOAT NOT NULL
-    );
-CREATE TABLE IF NOT EXISTS "Transactions" (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(50) NOT NULL,--MAX 50 bytes
-    userId INTEGER NOT NULL REFERENCES "User"(id),
-    crytoIdBought INTEGER NOT NULL,--BTC
-    cryptoIdSold INTEGER NOT NULL,--USDT
-    amountBought DOUBLE PRECISION NOT NULL,
-    amountSold DOUBLE PRECISION NOT NULL,
-    completedAt TIMESTAMP NOT NULL,
-    price DOUBLE PRECISION NOT NULL,
-    description varchar(250) NOT NULL--MAX 250 bytes
     );
 CREATE TABLE IF NOT EXISTS "Crypto" (
     id SERIAL PRIMARY KEY,
@@ -23,3 +11,15 @@ CREATE TABLE IF NOT EXISTS "Crypto" (
     description varchar(250) NOT NULL,--MAX 250 bytes
     logourl varchar(250) NOT NULL--MAX 250 bytes
 );
+CREATE TABLE IF NOT EXISTS "Transactions" (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(50) NOT NULL,--MAX 50 bytes
+    userId INTEGER NOT NULL REFERENCES "Users"(id),
+    crytoIdBought INTEGER NOT NULL REFERENCES "Crypto"(id),--BTC
+    cryptoIdSold INTEGER NOT NULL REFERENCES "Crypto"(id),--USDT
+    amountBought DOUBLE PRECISION NOT NULL,
+    amountSold DOUBLE PRECISION NOT NULL,
+    completedAt TIMESTAMP NOT NULL,
+    price DOUBLE PRECISION NOT NULL,
+    description varchar(250) NOT NULL--MAX 250 bytes
+    );
